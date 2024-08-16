a51bfe8c277bdcad5b695bbfed00a3097cd3929b
Jonatan Fransson <jonte.frasse@gmail.com>
2024-08-16 13:10:18 +0200

    Added clamp method

--- src/Util/Math.php
+++ src/Util/Math.php
@@ -6,8 +6,19 @@ namespace Projom\Util;
 
 class Math
 {
-    public static function isSubsetKey(array $subset, array $superset): bool 
+    public static function isSubsetKey(array $subset, array $superset): bool
     {
         return count(array_diff_key($subset, $superset)) === 0;
+    }
+
+    public static function clamp(float|int $min, float|int $max, float|int $value): float|int
+    {
+        if ($value < $min)
+            return $min;
+
+        if ($value > $max)
+            return $max;
+
+        return $value;
     }
 }
