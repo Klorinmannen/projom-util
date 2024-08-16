cbe0f645e11b3f22611d2801ef02412b519fe320
Jonatan Fransson <jonte.frasse@gmail.com>
2024-08-16 13:08:29 +0200

    Added array utils

---
+++ src/Util/Arrays.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Projom\Util;
+
+use Projom\Util\Strings;
+
+class Arrays
+{
+	public static function clean(array $list): array
+	{
+		return array_map([Strings::class, 'clean'], $list);
+	}
+
+	public static function join(array $list, string $delimeter = ','): string
+	{
+		return implode($delimeter, $list);
+	}
+
+	public static function flatten(array $list): array
+	{
+		return array_merge(...$list);
+	}
+
+	public static function merge(array ...$lists): array
+	{
+		return array_merge(...$lists);
+	}
+
+	public static function removeEmpty(array $list): array
+	{
+		return array_filter($list);
+	}
+}
