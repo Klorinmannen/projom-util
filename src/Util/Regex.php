84af110693219b728b39a8e4fa4ee70741c946a8
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2024-11-14 13:35:00 +0100

    Refactoring and development

--- src/Util/Regex.php
+++ src/Util/Regex.php
@@ -6,27 +6,49 @@ namespace Projom\Util;
 
 class Regex
 {
-    /*
-    * Rational numbers (Q) are a superset of Integers (Z).
-    * Therefore integers will also match.
-    */
-    const FLOAT_PATTERN = '/^-?[0-9]+([\.,]{1}[0-9]+)?$/';
+    private const FLOAT_PATTERN = '/^[+-]?[0-9]*[.,]{1}[0-9]+$/';
+    private const NUMBER_PATTERN = '/^[+-]?([0-9]*[.,]{1})?[0-9]+$/';
+    private const INT_PATTERN = '/^[+-]?[0-9]+$/';
+    private const INT_POS_PATTERN = '/^[+]?[0-9]+$/';
+    private const INT_NEG_PATTERN = '/^[-]{1}[0-9]+$/';
+    private const INT_ID_PATTERN = '/^[1-9]+[0-9]*$/';
+    private const TEXT_PATTERN = '/^.*$/';
+    private const QUERY_PATTERN = '/^[\w\/\.?=&]+$/';
+    private const SANTIZE_PATTERN = '/[^\w]+/';
 
-    const ID_PATTERN = '/^[1-9]+[0-9]*$/';
-    const INT_PATTERN = '/^-?[0-9]+$/';
-    const TEXT_PATTERN = '/^.*$/';
-    const QUERY_PATTERN = '/^[\w\/\.?=&]+$/';
-    const SANTIZE_PATTERN = '/[^\w]+/';
+    public static function matchFloat(int|float|string $subject): bool
+    {
+        $match = static::match(static::FLOAT_PATTERN, (string) $subject);
+        return $match;
+    }
 
-    public static function matchInteger(int $subject): bool
+    public static function matchNumber(int|float|string $subject): bool
+    {
+        $match = static::match(static::NUMBER_PATTERN, (string) $subject);
+        return $match;
+    }
+
+    public static function matchInteger(int|float|string $subject): bool
     {
         $match = static::match(static::INT_PATTERN, (string) $subject);
         return $match;
     }
 
-    public static function matchID(int $subject): bool
+    public static function matchPositiveInteger(int|float|string $subject): bool
     {
-        $match = static::match(static::ID_PATTERN, (string) $subject);
+        $match = static::match(static::INT_POS_PATTERN, (string) $subject);
+        return $match;
+    }
+
+    public static function matchNegativeInteger(int|float|string $subject): bool
+    {
+        $match = static::match(static::INT_NEG_PATTERN, (string) $subject);
+        return $match;
+    }
+
+    public static function matchIntegerID(int|float|string $subject): bool
+    {
+        $match = static::match(static::INT_ID_PATTERN, (string) $subject);
         return $match;
     }
 
@@ -42,22 +64,19 @@ class Regex
         return $match;
     }
 
-    public static function matchFloat(float $subject): bool
-    {
-        $match = static::match(static::FLOAT_PATTERN, (string) $subject);
-        return $match;
-    }
-
-    public static function match(string $pattern, string $subject): bool
+    public static function match(string $pattern, int|float|string $subject): bool
     {
         return preg_match($pattern, $subject) === 1;
     }
 
-    public static function pattern(string $pattern, string $subject): array
+    /**
+     * Returns the matches of the pattern in the subject. If no match is found, null is returned.
+     */
+    public static function pattern(string $pattern, string $subject): null|array
     {
         if (preg_match($pattern, $subject, $matches) === 1)
             return $matches;
-        return [];
+        return null;
     }
 
     public static function sanitize(string $subject): string
